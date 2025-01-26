fdbde2ea13bff76f875ed2b82ca0435e3627643e
Yura Luchka <lluchkaa@gmail.com>
2025-01-26 20:11:32 +0000

    add empty flake.nix

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Nix and NixOS system and tools by lluchkaa";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+
+
+  };
+}
