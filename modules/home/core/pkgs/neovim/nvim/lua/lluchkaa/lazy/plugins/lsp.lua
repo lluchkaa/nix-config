0fc6f1f2f98040b0b157904d3d5e52856cdb439f
Yura Luchka <lluchkaa@gmail.com>
2025-10-04 16:35:59 +0300

    update lsps

--- modules/home/core/pkgs/neovim/nvim/lua/lluchkaa/lazy/plugins/lsp.lua
+++ modules/home/core/pkgs/neovim/nvim/lua/lluchkaa/lazy/plugins/lsp.lua
@@ -167,23 +167,20 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        gopls = {},
-        rust_analyzer = {},
-        ts_ls = {},
+        gopls = { filetypes = { "go", "gomod" } },
+        rust_analyzer = { filetypes = { "rust" } },
+        ts_ls = { filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" } },
         html = { filetypes = { "html" } },
-        -- eslint = {},
+        ruff = { filetypes = { "python" } },
+        pyright = { filetypes = { "python" } },
 
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
+          filetypes = { "lua" },
           settings = {
             Lua = {
               completion = {
                 callSnippet = "Replace",
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
         },
