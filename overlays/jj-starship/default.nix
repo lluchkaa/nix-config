bdee7b50215298a4e53ff5a8389a49202de1f2fe
Yura Luchka <lluchkaa@gmail.com>
2026-04-05 21:17:46 +0300

    fix unused var

--- overlays/jj-starship/default.nix
+++ overlays/jj-starship/default.nix
@@ -1,5 +1,5 @@
 { jj-starship }:
-final: prev: {
+final: _: {
   jj-starship = jj-starship.packages.${final.stdenv.hostPlatform.system}.jj-starship;
   jj-starship-no-git = jj-starship.packages.${final.stdenv.hostPlatform.system}.jj-starship-no-git;
 }
